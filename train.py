d5ccea0ea0c0f0aad30ef148692cdf344d0377d5
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-05-31 17:45:04 +0200

    Fixes on the net definition + Full data loading

--- train.py
+++ train.py
@@ -1,11 +1,13 @@
 import os
 import matplotlib
 import numpy as np
+from theano import tens
 from scipy import ndimage as nd
 from nibabel import load as load_nii
 from sklearn.cross_validation import train_test_split
 from lasagne.layers import InputLayer, DenseLayer, DropoutLayer, FeaturePoolLayer, InverseLayer
 from lasagne.layers.dnn import Conv3DDNNLayer, MaxPool3DDNNLayer
+from lasagne.layers import Layer
 from lasagne import nonlinearities
 from lasagne import objectives
 from lasagne import updates
@@ -21,78 +23,113 @@ from nolearn_utils.hooks import (
     SaveTrainingHistory, PlotTrainingHistory,
     EarlyStopping
 )
+
 matplotlib.use('Agg')
 
 
-def load_data(test_size=0.25, random_state=None, dir_name='~/DATA/Challenge',
+class Unpooling3D(Layer):
+    def __init__(self, incoming, poolsize=2, ignore_border=True, **kwargs):
+        super(Unpooling3D, self).__init__(incoming, **kwargs)
+        self.poolsize = poolsize
+        self.ignore_border = ignore_border
+
+    def get_output_for(self, input, **kwargs):
+        output = input.repeat(self.poolsize, axis=2).repeat(self.poolsize, axis=4).repeat(self.poolsize, axis=5)
+        return output
+
+    def get_output_shape_for(self, input_shape):
+        return tuple(a*self.poolsize for a in input_shape[-2:])
+
+
+
+
+def load_data(test_size=0.25, random_state=None, dir_name='/home/mariano/DATA/Challenge/',
               flair_name='FLAIR_preprocessed.nii.gz', pd_name='DP_preprocessed.nii.gz',
               t2_name='T2_preprocessed.nii.gz', gado_name='GADO_preprocessed.nii.gz',
               t1_name='T1_preprocessed.nii.gz', use_flair=True, use_pd=True, use_t2=True,
-              use_gado=True, use_t1=True
+              use_gado=True, use_t1=True, rater=3
               ):
-    # Setting up the lists for all images
+
     patients = [file for file in sorted(os.listdir(dir_name)) if os.path.isdir(os.path.join(dir_name, file))]
-    n_patients = len(patients)
-    flair = None
-    pd = None
-    t2 = None
-    gado = None
-    t1 = None
 
-    # We load the image modalities for each patient according to the parameters
-    if use_flair:
-        flair = [load_nii('%s/%s/%s' % (dir_name, patient, flair_name)).get_data() for patient in patients]
-    if use_pd:
-        pd = np.asarray([load_nii('%s/%s/%s' % (dir_name, patient, pd_name)).get_data() for patient in patients])
-    if use_t2:
-        t2 = np.asarray([load_nii('%s/%s/%s' % (dir_name, patient, t2_name)).get_data() for patient in patients])
-    if use_gado:
-        gado = np.asarray([load_nii('%s/%s/%s' % (dir_name, patient, gado_name)).get_data() for patient in patients])
-    if use_t1:
-        t1 = np.asarray([load_nii('%s/%s/%s' % (dir_name, patient, t1_name)).get_data() for patient in patients])
+    try:
+        y = np.load('%slabel%d_vector.npy' % (dir_name, rater))
+    except IOError:
+        masks = [load_nii('%s/%s/ManualSegmentation_%d.nii.gz' % (dir_name, patient, rater)).get_data() for patient in
+                 patients]
+        min_shape = min([im.shape for im in masks])
+        y = np.asarray(
+            [nd.zoom((im - im.mean()) / im.std(),
+                     [float(min_shape[0]) / im.shape[0], float(min_shape[1]) / im.shape[1],
+                      float(min_shape[2]) / im.shape[2]]) for im in masks]
+        ).astype(np.int32)
+        np.save('%slabel%d_vector.npy' % (dir_name, rater), y)
 
-    # The size of the final numpy array should be: n_patients x n_images x size_of_image
-    if use_flair:
-        min_shape = min([im.shape for im in flair])
-        flair = np.asarray([nd.zoom(im, [float(min_shape[0]) / im.shape[0], float(min_shape[1]) / im.shape[1],
-                                         float(min_shape[2]) / im.shape[2]]) for im in flair])
-        flair.reshape(n_patients, 1, flair.shape[1], flair.shape[2], flair.shape[3]).astype(np.float32)
-    if use_pd:
-        min_shape = min([im.shape for im in pd])
-        pd = np.asarray([nd.zoom(im, [float(min_shape[0]) / im.shape[0], float(min_shape[1]) / im.shape[1],
-                                      float(min_shape[2]) / im.shape[2]]) for im in pd])
-        pd.reshape(n_patients, 1, pd.shape[1], pd.shape[2], pd.shape[3]).astype(np.float32)
-    if use_t2:
-        min_shape = min([im.shape for im in t2])
-        t2 = np.asarray([nd.zoom(im, [float(min_shape[0]) / im.shape[0], float(min_shape[1]) / im.shape[1],
-                                      float(min_shape[2]) / im.shape[2]]) for im in t2])
-        t2.reshape(n_patients, 1, t2.shape[1], t2.shape[2], t2.shape[3]).astype(np.float32)
-    if use_gado:
-        min_shape = min([im.shape for im in gado])
-        gado = np.asarray([nd.zoom(im, [float(min_shape[0]) / im.shape[0], float(min_shape[1]) / im.shape[1],
-                                        float(min_shape[2]) / im.shape[2]]) for im in gado])
-        gado.reshape(n_patients, 1, gado.shape[1], gado.shape[2], gado.shape[3]).astype(np.float32)
-    if use_t1:
-        min_shape = min([im.shape for im in t1])
-        t1 = np.asarray([nd.zoom(im, [float(min_shape[0]) / im.shape[0], float(min_shape[1]) / im.shape[1],
-                                      float(min_shape[2]) / im.shape[2]]) for im in t1])
-        t1.reshape(n_patients, 1, t1.shape[1], t1.shape[2], t1.shape[3]).astype(np.float32)
-    images = filter(None, [flair, pd, t2, gado, t1])
-    image_vector = np.stack(images, axis=1)
-    n_images = len(images)
-    x_len = image_vector.shape[2]
-    y_len = image_vector.shape[3]
-    z_len = image_vector.shape[4]
+    try:
+        X = np.load(dir_name + 'image_vector.npy')
+    except IOError:
+        # Setting up the lists for all images
+        n_patients = len(patients)
+        flair = None
+        pd = None
+        t2 = None
+        gado = None
+        t1 = None
 
-    df = pd.read_csv('examples/mnist/train.csv')
-    X = df[df.columns[1:]].values.reshape(-1, 1, 28, 28).astype(np.float32)
-    X = X / 255
-    y = df['label'].values.astype(np.int32)
+        # We load the image modalities for each patient according to the parameters
+        if use_flair:
+            images = [load_nii('%s/%s/%s' % (dir_name, patient, flair_name)).get_data() for patient in patients]
+            min_shape = min([im.shape for im in images])
+            flair = np.asarray(
+                [nd.zoom((im - im.mean()) / im.std(),
+                         [float(min_shape[0]) / im.shape[0], float(min_shape[1]) / im.shape[1],
+                          float(min_shape[2]) / im.shape[2]]) for im in images]
+            )
+            flair.reshape(n_patients, 1, flair.shape[1], flair.shape[2], flair.shape[3]).astype(np.float32)
+        if use_pd:
+            images = [load_nii('%s/%s/%s' % (dir_name, patient, pd_name)).get_data() for patient in patients]
+            min_shape = min([im.shape for im in images])
+            pd = np.asarray(
+                [nd.zoom((im - im.mean()) / im.std(),
+                         [float(min_shape[0]) / im.shape[0], float(min_shape[1]) / im.shape[1],
+                          float(min_shape[2]) / im.shape[2]]) for im in images]
+            )
+            pd.reshape(n_patients, 1, pd.shape[1], pd.shape[2], pd.shape[3]).astype(np.float32)
+        if use_t2:
+            images = [load_nii('%s/%s/%s' % (dir_name, patient, t2_name)).get_data() for patient in patients]
+            min_shape = min([im.shape for im in images])
+            t2 = np.asarray(
+                [nd.zoom((im - im.mean()) / im.std(),
+                         [float(min_shape[0]) / im.shape[0], float(min_shape[1]) / im.shape[1],
+                          float(min_shape[2]) / im.shape[2]]) for im in images]
+            )
+            t2.reshape(n_patients, 1, t2.shape[1], t2.shape[2], t2.shape[3]).astype(np.float32)
+        if use_gado:
+            images = [load_nii('%s/%s/%s' % (dir_name, patient, gado_name)).get_data() for patient in patients]
+            min_shape = min([im.shape for im in images])
+            gado = np.asarray(
+                [nd.zoom((im - im.mean()) / im.std(),
+                         [float(min_shape[0]) / im.shape[0], float(min_shape[1]) / im.shape[1],
+                          float(min_shape[2]) / im.shape[2]]) for im in images]
+            )
+            gado.reshape(n_patients, 1, gado.shape[1], gado.shape[2], gado.shape[3]).astype(np.float32)
+        if use_t1:
+            images = [load_nii('%s/%s/%s' % (dir_name, patient, t1_name)).get_data() for patient in patients]
+            min_shape = min([im.shape for im in images])
+            t1 = np.asarray(
+                [nd.zoom((im - im.mean()) / im.std(),
+                         [float(min_shape[0]) / im.shape[0], float(min_shape[1]) / im.shape[1],
+                          float(min_shape[2]) / im.shape[2]]) for im in images]
+            )
+            t1.reshape(n_patients, 1, t1.shape[1], t1.shape[2], t1.shape[3]).astype(np.float32)
+
+        X = np.stack([data for data in [flair, pd, t2, gado, t1] if data is not None], axis=1)
+        np.save(dir_name + 'image_vector.npy', X)
+
+    # y = df['label'].values.astype(np.int32)
+
     return train_test_split(X, y, test_size=test_size, random_state=random_state)
 
-
-image_size = 28
-batch_size = 1024
 
 train_iterator_mixins = [
     ShuffleBatchIteratorMixin,
@@ -107,7 +144,7 @@ test_iterator_mixins = [
 TestIterator = make_iterator('TestIterator', test_iterator_mixins)
 
 train_iterator_kwargs = {
-    'batch_size': batch_size,
+    'batch_size': 15,
     'buffer_size': 5,
     'affine_p': 0.5,
     'affine_scale_choices': np.linspace(0.75, 1.25, 5),
@@ -117,7 +154,7 @@ train_iterator_kwargs = {
 train_iterator = TrainIterator(**train_iterator_kwargs)
 
 test_iterator_kwargs = {
-    'batch_size': batch_size,
+    'batch_size': 15,
     'buffer_size': 5,
 }
 test_iterator = TestIterator(**test_iterator_kwargs)
@@ -127,21 +164,27 @@ save_training_history = SaveTrainingHistory('./examples/mnist/model_history.pkl'
 plot_training_history = PlotTrainingHistory('./examples/mnist/training_history.png')
 early_stopping = EarlyStopping(patience=100)
 
+pool_l = (MaxPool3DDNNLayer, {'name': 'pool', 'pool_size': 2})
+deconv2_l = (InverseLayer, {'name': 'deconv2', 'incoming': conv2_l, 'layer': conv2_l})
+unpool_l = (InverseLayer, {'name': 'unpool', 'incoming': deconv2_l, 'layer': pool_l})
+deconv1_l = (InverseLayer, {'name': 'deconv1', 'incoming': unpool_l, 'layer': conv1_l})
+
 net = NeuralNet(
     layers=[
-        (InputLayer, dict(name='input', shape=(None, 1, image_size, image_size))),
-        (Conv3DDNNLayer, dict(name='conv1', num_filters=32, filter_size=(9, 9, 9), pad='same')),
-        (MaxPool3DDNNLayer, dict(name='pool', pool_size=2)),
+        (InputLayer, {'name': 'input', 'shape': (None, 5, None, None, None)}),
 
-        (Conv3DDNNLayer, dict(name='conv2', num_filters=32, filter_size=(9, 9, 9), pad='same')),
+        (Conv3DDNNLayer, {'name': 'conv1', 'num_filters': 32, 'filter_size': (9, 9, 9)}),
+        (MaxPool3DDNNLayer, {'name': 'pool', 'pool_size': 2}),
 
-        (InverseLayer, dict(name='deconv2', incoming='conv2', layer='conv2')),
-        (InverseLayer, dict(name='unpool', incoming='deconv2', layer='pool')),
-        (InverseLayer, dict(name='deconv1', incoming='unpool', layer='pool')),
+        (Conv3DDNNLayer, {'name': 'conv2', 'num_filters': 32, 'filter_size': (9, 9, 9)}),
+
+        (Conv3DDNNLayer, {'name': 'conv2', 'num_filters': 9, 'filter_size': (9, 9, 32)}),
+        (Unpooling3D, {'name': 'unpool', 'pool_size': 2})
+        deconv1_l,
 
     ],
 
-    regression=False,
+    regression=True,
     objective_loss_function=objectives.categorical_crossentropy,
 
     update=updates.adadelta,
