9136681455b2843db1c1deaf57f26d89143f375f
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-06-01 17:02:06 +0200

    - Added parameters to predict

--- predict.py
+++ predict.py
@@ -12,12 +12,18 @@ if __name__ == '__main__':
                       help="read data from FOLDER")
     parser.add_option('-v', '--verbose',
                       action='store_true', dest='verbose', default=False)
+    parser.add_option('-c', '--convolution-size',
+                      action='store', dest='convo_size', type='int', nargs=1, default=15)
+    parser.add_option('-p', '--pool-size',
+                      action='store', dest='pool_size', type='int', nargs=1, default=2)
+    parser.add_option('-t', '--test-size',
+                      action='store', dest='test_size', type='float', nargs=1, default=2)
     (options, args) = parser.parse_args()
 
     X = np.load(options.folder + 'test_encoder.npy')
     X_train = np.load(options.folder + 'train_encoder.npy')
 
-    net = create_encoder(X_train.shape, 15, 2)
+    net = create_encoder(X_train.shape, options.convo_size, options.pool_size)
     net.load_params_from('./model_weights.pkl')
     y_pred = net.predict(X)
 
