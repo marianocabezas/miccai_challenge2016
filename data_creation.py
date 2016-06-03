534b367aac42f07efa29c3f21e1b7c4568f34f3c
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-06-03 11:49:49 +0200

    - Fixed weak arguments from PyCharm and PEP8

--- data_creation.py
+++ data_creation.py
@@ -6,7 +6,7 @@ from nibabel import load as load_nii
 from nibabel import save as save_nii
 from nibabel import Nifti2Image as NiftiImage
 from math import floor
-#from sklearn.cross_validation import train_test_split
+
 
 def train_test_split(data, labels, test_size=0.1, random_state=42):
     # Init (Set the random seed and determine the number of cases for test)
@@ -21,14 +21,14 @@ def train_test_split(data, labels, test_size=0.1, random_state=42):
     np.random.seed(random_state)
     shuffled_labels = np.random.permutation(labels)
 
-    X_train = shuffled_data[:-n_test]
-    X_test = shuffled_data[-n_test:]
+    x_train = shuffled_data[:-n_test]
+    x_test = shuffled_data[-n_test:]
     y_train = shuffled_labels[:-n_test]
     y_test = shuffled_data[-n_test:]
     idx_train = indices[:-n_test]
     idx_test = indices[-n_test:]
 
-    return X_train, X_test, y_train, y_test, idx_train, idx_test
+    return x_train, x_test, y_train, y_test, idx_train, idx_test
 
 
 def reshape_save_nifti(image, original_name):
@@ -53,7 +53,7 @@ def reshape_save_nifti(image, original_name):
 
 def load_and_vectorize(name, dir_name, datatype=np.float32):
     # Get the names of the images and load them
-    patients = [file for file in sorted(os.listdir(dir_name)) if os.path.isdir(os.path.join(dir_name, file))]
+    patients = [f for f in sorted(os.listdir(dir_name)) if os.path.isdir(os.path.join(dir_name, f))]
     image_names = ['%s/%s/%s' % (dir_name, patient, name) for patient in patients]
     images = [load_nii(image_name).get_data() for image_name in image_names]
     # Reshape everything to have data of homogenous size (important for training)
@@ -68,18 +68,23 @@ def load_and_vectorize(name, dir_name, datatype=np.float32):
     return data.reshape([data.shape[0], 1, data.shape[1], data.shape[2], data.shape[3]]).astype(datatype), image_names
 
 
-def load_images(test_size=0.25, dir_name='/home/mariano/DATA/Challenge/',
-                  flair_name='FLAIR_preprocessed.nii.gz', pd_name='DP_preprocessed.nii.gz',
-                  t2_name='T2_preprocessed.nii.gz', gado_name='GADO_preprocessed.nii.gz',
-                  t1_name='T1_preprocessed.nii.gz', use_flair=True, use_pd=True, use_t2=True,
-                  use_gado=True, use_t1=True
-                ):
-
-    patients = [dir_name + file for file in sorted(os.listdir(dir_name)) if os.path.isdir(os.path.join(dir_name, file))]
+def load_images(
+        dir_name,
+        flair_name,
+        pd_name,
+        t2_name,
+        gado_name,
+        t1_name,
+        use_flair,
+        use_pd,
+        use_t2,
+        use_gado,
+        use_t1
+):
 
     try:
-        X = np.load(dir_name + 'image_vector.npy')
-        image_names = np.load(dir_name + 'image_names_encoder.npy')
+        x = np.load(dir_name + 'image_vector.npy')
+        np.load(dir_name + 'image_names_encoder.npy')
     except IOError:
         # Setting up the lists for all images
         flair, flair_names = None, None
@@ -100,25 +105,37 @@ def load_images(test_size=0.25, dir_name='/home/mariano/DATA/Challenge/',
         if use_t1:
             t1, t1_names = load_and_vectorize(t1_name, dir_name)
 
-        X = np.stack([data for data in [flair, pd, t2, gado, t1] if data is not None], axis=1)
-        image_names = np.stack([name for name in [flair_names, pd_names, t2_names, gado_names, t1_names] if name is not None])
-        np.save(dir_name + 'image_vector_encoder.npy', X)
+        x = np.stack([data for data in [flair, pd, t2, gado, t1] if data is not None], axis=1)
+        image_names = np.stack([name for name in [
+                flair_names,
+                pd_names,
+                t2_names,
+                gado_names,
+                t1_names
+        ] if name is not None])
+        np.save(dir_name + 'image_vector_encoder.npy', x)
         np.save(dir_name + 'image_names_encoder.npy', image_names)
 
-    return X
+    return x
 
 
+def load_encoder_data(
+        test_size=0.25,
+        random_state=None,
+        dir_name='/home/mariano/DATA/Challenge/',
+        flair_name='FLAIR_preprocessed.nii.gz',
+        pd_name='DP_preprocessed.nii.gz',
+        t2_name='T2_preprocessed.nii.gz',
+        gado_name='GADO_preprocessed.nii.gz',
+        t1_name='T1_preprocessed.nii.gz',
+        use_flair=True,
+        use_pd=True,
+        use_t2=True,
+        use_gado=True,
+        use_t1=True
+):
 
-def load_encoder_data(test_size=0.25, random_state=None, dir_name='/home/mariano/DATA/Challenge/',
-                  flair_name='FLAIR_preprocessed.nii.gz', pd_name='DP_preprocessed.nii.gz',
-                  t2_name='T2_preprocessed.nii.gz', gado_name='GADO_preprocessed.nii.gz',
-                  t1_name='T1_preprocessed.nii.gz', use_flair=True, use_pd=True, use_t2=True,
-                  use_gado=True, use_t1=True
-                  ):
-
-
-    X = load_images(
-        test_size,
+    x = load_images(
         dir_name,
         flair_name,
         pd_name,
@@ -131,22 +148,32 @@ def load_encoder_data(test_size=0.25, random_state=None, dir_name='/home/mariano
         use_gado,
         use_t1
     )
-    y = np.reshape(X, [X.shape[0], -1])
+    y = np.reshape(x, [x.shape[0], -1])
 
-    return train_test_split(X, y, test_size=test_size, random_state=random_state)
+    return train_test_split(x, y, test_size=test_size, random_state=random_state)
 
 
-def load_unet_data(test_size=0.25, random_state=None, dir_name='/home/mariano/DATA/Challenge/',
-              flair_name='FLAIR_preprocessed.nii.gz', pd_name='DP_preprocessed.nii.gz',
-              t2_name='T2_preprocessed.nii.gz', gado_name='GADO_preprocessed.nii.gz',
-              t1_name='T1_preprocessed.nii.gz', use_flair=True, use_pd=True, use_t2=True,
-              use_gado=True, use_t1=True, rater=3
-              ):
+def load_unet_data(
+        test_size=0.25,
+        random_state=None,
+        dir_name='/home/mariano/DATA/Challenge/',
+        flair_name='FLAIR_preprocessed.nii.gz',
+        pd_name='DP_preprocessed.nii.gz',
+        t2_name='T2_preprocessed.nii.gz',
+        gado_name='GADO_preprocessed.nii.gz',
+        t1_name='T1_preprocessed.nii.gz',
+        use_flair=True,
+        use_pd=True,
+        use_t2=True,
+        use_gado=True,
+        use_t1=True,
+        rater=3
+):
 
     try:
         y = np.load('%slabel%d_vector.npy' % (dir_name, rater))
     except IOError:
-        patients = [file for file in sorted(os.listdir(dir_name)) if os.path.isdir(os.path.join(dir_name, file))]
+        patients = [f for f in sorted(os.listdir(dir_name)) if os.path.isdir(os.path.join(dir_name, f))]
         masks = [load_nii('%s/%s/ManualSegmentation_%d.nii.gz' % (dir_name, patient, rater)).get_data() for patient in
                  patients]
         min_shape = min([im.shape for im in masks])
@@ -157,8 +184,7 @@ def load_unet_data(test_size=0.25, random_state=None, dir_name='/home/mariano/DA
         ).astype(np.uint8)
         np.save('%slabel%d_vector.npy' % (dir_name, rater), y)
 
-    X = load_images(
-        test_size,
+    x = load_images(
         dir_name,
         flair_name,
         pd_name,
@@ -172,4 +198,4 @@ def load_unet_data(test_size=0.25, random_state=None, dir_name='/home/mariano/DA
         use_t1
     )
 
-    return train_test_split(X, y, test_size=test_size, random_state=random_state)
+    return train_test_split(x, y, test_size=test_size, random_state=random_state)
