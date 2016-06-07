d61b5a1c7101ba1c9d67d5820bc3524f1ab07411
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-06-07 16:13:38 +0200

    - Bug fix

--- nets.py
+++ nets.py
@@ -30,10 +30,6 @@ def create_encoder_string(forward_layers, input_shape, convo_size, pool_size, di
     c_index = Index()
     p_index = Index()
 
-    save_weights = SaveWeights(dir_name + 'model_weights.pkl', only_best=True, pickle=False)
-    save_training_history = SaveTrainingHistory(dir_name + 'model_history.pkl')
-    plot_training_history = PlotTrainingHistory(dir_name + 'training_history.png')
-    early_stopping = EarlyStopping(patience=100)
     # These are all the possible layers for our autoencoders
     possible_layers = {
         'i': '(InputLayer, {\'name\': \'input\',\'shape\': input_shape})',
@@ -50,10 +46,10 @@ def create_encoder_string(forward_layers, input_shape, convo_size, pool_size, di
              '\'name\': \'pool%d\' % (p_index.inc()),'
              '\'pool_size\': pool_size})',
         'u': '(Unpooling3D, {'
-             '\'name\': \'unpool%d\' % (p_index_dec()),'
+             '\'name\': \'unpool%d\' % (p_index.dec()),'
              '\'pool_size\': pool_size})',
         'd': '(Conv3DDNNLayer, {'
-             '\'name\': \'deconv%d\' % (c_index_dec()),'
+             '\'name\': \'deconv%d\' % (c_index.dec()),'
              '\'num_filters\': number_filters,'
              '\'filter_size\': (convo_size, convo_size, convo_size),'
              '\'pad\': \'full\'})',
@@ -66,15 +62,21 @@ def create_encoder_string(forward_layers, input_shape, convo_size, pool_size, di
     }
 
     # We create the backwards patt of the encoder from the forward path
+    # We need to mirror the configuration of the layers and change the pooling operators with unpooling,
+    # and the convolutions with deconvolutions (convolutions with diferent padding). This definitions
+    # match the values of the possible_layers dictionary
     back_layers = ''.join(['d' if l is 'c' else 'u' for l in forward_layers[::-1]])
     last_conv = back_layers.rfind('d')
     back_layers = back_layers[:last_conv] + 'f' + back_layers[last_conv+1:]
     # We create the final string defining the net with the necessary input and reshape layers
-    final_layers = forward_layers + back_layers
-    if 'i' not in final_layers:
-        forward_layers = 'i' + forward_layers
-    if 'r' not in final_layers:
-        forward_layers += 'r'
+    # We assume that the user will never put these parameters as part of the net definition when
+    # calling the main python function
+    final_layers = 'i' + forward_layers + back_layers + 'r'
+
+    save_weights = SaveWeights(dir_name + 'model_weights.pkl', only_best=True, pickle=False)
+    save_training_history = SaveTrainingHistory(dir_name + 'model_history.pkl')
+    plot_training_history = PlotTrainingHistory(dir_name + 'training_history.png')
+    early_stopping = EarlyStopping(patience=100)
 
     encoder = NeuralNet(
         layers=[eval(possible_layers[l]) for l in final_layers],
