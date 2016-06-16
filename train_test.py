50f1e884b7fbf0bcaacd524813a16ceef443dfba
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-06-16 10:12:35 +0200

    - Deleted matplotlib (I wasn't using it) - Created a main() function - Fixed some PEP8

--- train_test.py
+++ train_test.py
@@ -1,15 +1,72 @@
 import os
 from optparse import OptionParser
-import matplotlib
 import numpy as np
 import cPickle
 from data_creation import load_unet_data, load_encoder_data
 from data_creation import reshape_save_nifti_to_dir, reshape_save_nifti
 from nets import create_unet3D_string, create_encoder3D_string
-matplotlib.use('Agg')
 
 
-def unet3D(options):
+def main():
+    # Parse command line options
+    usage = "usage: %prog [options] arg"
+    parser = OptionParser(usage)
+    parser.add_option('-f', '--folder', dest='folder',
+                      default='/home/mariano/DATA/Challenge/',
+                      help="read data from FOLDER")
+    parser.add_option('-v', '--verbose',
+                      action='store_true', dest='verbose', default=False)
+    parser.add_option('-c', '--convolution-size',
+                      action='store', dest='convo_size', type='int', nargs=1, default=3)
+    parser.add_option('-p', '--pool-size',
+                      action='store', dest='pool_size', type='int', nargs=1, default=2)
+    parser.add_option('-t', '--test-size',
+                      action='store', dest='test_size', type='float', nargs=1, default=0.25)
+    parser.add_option('-n', '--number-filters',
+                      action='store', dest='number_filters', type='int', nargs=1, default=4)
+    parser.add_option('-l', '--forward-layers',
+                      action='store', dest='layers', type='string', nargs=1, default='cac')
+    parser.add_option('-w', '--window-size',
+                      action='store', dest='patch_size', type='int', nargs=3, default=(15, 15, 15))
+    parser.add_option('-i', '--image-size',
+                      action='store', dest='min_shape', type='int', nargs=3, default=None)
+    parser.add_option('--use-gado',
+                      action='store_true', dest='use_gado')
+    parser.add_option('--no-gado',
+                      action='store_false', dest='use_gado', default=False)
+    parser.add_option('--gado',
+                      action='store', dest='gado', type='string', default='GADO_preprocessed.nii.gz')
+    parser.add_option('--use-flair',
+                      action='store_true', dest='use_flair')
+    parser.add_option('--no-flair',
+                      action='store_false', dest='use_flair', default=True)
+    parser.add_option('--flair',
+                      action='store', dest='flair', type='string', default='FLAIR_preprocessed.nii.gz')
+    parser.add_option('--use-pd',
+                      action='store_true', dest='use_pd')
+    parser.add_option('--no-pd',
+                      action='store_false', dest='use_pd', default=True)
+    parser.add_option('--pd',
+                      action='store', dest='pd', type='string', default='DP_preprocessed.nii.gz')
+    parser.add_option('--use-t2',
+                      action='store_true', dest='use_t2')
+    parser.add_option('--no-t2',
+                      action='store_false', dest='use_t2', default=True)
+    parser.add_option('--t2',
+                      action='store', dest='t2', type='string', default='T2_preprocessed.nii.gz')
+    parser.add_option('--use-t1',
+                      action='store_true', dest='use_t1')
+    parser.add_option('--no-t1',
+                      action='store_false', dest='use_t1', default=True)
+    parser.add_option('--t1',
+                      action='store', dest='t1', type='string', default='T1_preprocessed.nii.gz')
+    parser.add_option('--mask',
+                      action='store', dest='mask', type='string', default='Consensus.nii.gz')
+
+    (options, args) = parser.parse_args()
+
+
+def unet3d(options):
     g = '\033[32m'
     bg = '\033[32;1m'
     b = '\033[1m'
@@ -73,7 +130,8 @@ def unet3D(options):
     images_names = [(y_im, image_names[1, idx]) for y_im, idx in zip(y, idx_test)]
     [reshape_save_nifti_to_dir(im, name) for (im, name) in images_names]
 
-def unet_patches3D(options):
+
+def unet_patches3d(options):
     g = '\033[32m'
     bg = '\033[32;1m'
     b = '\033[1m'
@@ -139,7 +197,7 @@ def unet_patches3D(options):
     [reshape_save_nifti_to_dir(im, name) for (im, name) in images_names]
 
 
-def autoencoder3D(options):
+def autoencoder3d(options):
     g = '\033[32m'
     bg = '\033[32;1m'
     b = '\033[1m'
@@ -205,59 +263,6 @@ def autoencoder3D(options):
 
 
 if __name__ == '__main__':
-    # Parse command line options
-    usage = "usage: %prog [options] arg"
-    parser = OptionParser(usage)
-    parser.add_option('-f', '--folder', dest='folder',
-                      default='/home/mariano/DATA/Challenge/',
-                      help="read data from FOLDER")
-    parser.add_option('-v', '--verbose',
-                      action='store_true', dest='verbose', default=False)
-    parser.add_option('-c', '--convolution-size',
-                      action='store', dest='convo_size', type='int', nargs=1, default=3)
-    parser.add_option('-p', '--pool-size',
-                      action='store', dest='pool_size', type='int', nargs=1, default=2)
-    parser.add_option('-t', '--test-size',
-                      action='store', dest='test_size', type='float', nargs=1, default=0.25)
-    parser.add_option('-n', '--number-filters',
-                      action='store', dest='number_filters', type='int', nargs=1, default=4)
-    parser.add_option('-l', '--forward-layers',
-                      action='store', dest='layers', type='string', nargs=1, default='cac')
-    parser.add_option('-i', '--image-size',
-                      action='store', dest='min_shape', type='int', nargs=3, default=None)
-    parser.add_option('--use-gado',
-                      action='store_true', dest='use_gado', default=False)
-    parser.add_option('--no-gado',
-                      action='store_false', dest='use_gado', default=False)
-    parser.add_option('--gado',
-                      action='store', dest='gado', type='string', default='GADO_preprocessed.nii.gz')
-    parser.add_option('--use-flair',
-                      action='store_true', dest='use_flair', default=True)
-    parser.add_option('--no-flair',
-                      action='store_false', dest='use_flair', default=True)
-    parser.add_option('--flair',
-                      action='store', dest='flair', type='string', default='FLAIR_preprocessed.nii.gz')
-    parser.add_option('--use-pd',
-                      action='store_true', dest='use_pd', default=True)
-    parser.add_option('--no-pd',
-                      action='store_false', dest='use_pd', default=True)
-    parser.add_option('--pd',
-                      action='store', dest='pd', type='string', default='DP_preprocessed.nii.gz')
-    parser.add_option('--use-t2',
-                      action='store_true', dest='use_t2', default=True)
-    parser.add_option('--no-t2',
-                      action='store_false', dest='use_t2', default=True)
-    parser.add_option('--t2',
-                      action='store', dest='t2', type='string', default='T2_preprocessed.nii.gz')
-    parser.add_option('--use-t1',
-                      action='store_true', dest='use_t1', default=True)
-    parser.add_option('--no-t1',
-                      action='store_false', dest='use_t1', default=True)
-    parser.add_option('--t1',
-                      action='store', dest='t1', type='string', default='T1_preprocessed.nii.gz')
-    parser.add_option('--mask',
-                      action='store', dest='mask', type='string', default='Consensus.nii.gz')
-
-    (options, args) = parser.parse_args()
+    main()
 
 
