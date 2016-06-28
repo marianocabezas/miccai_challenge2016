8b81275886d567cdfeddfff3f21b747482a2f921
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-06-28 11:44:39 +0200

    - Added CNN for patches

--- nets.py
+++ nets.py
@@ -57,48 +57,106 @@ def get_layers_string(net_layers, input_shape, convo_size, pool_size, number_fil
 
     # These are all the possible layers for our autoencoders
     possible_layers = {
-        'i': ('input', '(InputLayer, {'
-             '\'name\': \'\033[30minput\033[0m\','
-             '\'shape\': input_shape})'),
-        'c': ('\'conv%d\' % c_index.inc()', '(Conv3DDNNLayer, {'
-             '\'name\': \'\033[34mconv%d\033[0m\' % (c_index.inc()),'
-             '\'num_filters\': number_filters,'
-             '\'filter_size\': (convo_size, convo_size, convo_size),'
-             '\'pad\': \'valid\'})'),
-        'a': ('\'avg_pool%d\' % p_index.inc()', '(Pool3DDNNLayer, {'
-             '\'name\': \'\033[31mavg_pool%d\033[0m\' % (p_index.inc()),'
-             '\'pool_size\': pool_size,'
-             '\'mode\': \'average_inc_pad\'})'),
-        'm': ('\'max_pool%d\' % p_index.inc()', '(MaxPool3DDNNLayer, {'
-             '\'name\': \'\033[31mmax_pool%d\033[0m\' % (p_index.inc()),'
-             '\'pool_size\': pool_size})'),
-        'u': ('\'unpool%d\' % p_index.dec()', '(Unpooling3D, {'
-             '\'name\': \'\033[35munpool%d\033[0m\' % (p_index.dec()),'
-             '\'pool_size\': pool_size})'),
-        'd': ('\'deconv%d\' % c_index.dec()', '(Conv3DDNNLayer, {'
-             '\'name\': \'\033[36mdeconv%d\033[0m\' % (c_index.dec()),'
-             '\'num_filters\': number_filters,'
-             '\'filter_size\': (convo_size, convo_size, convo_size),'
-             '\'pad\': \'full\'})'),
-        'f': ('final', '(Conv3DDNNLayer, {'
-             '\'name\': \'\033[36mfinal\033[0m\','
-             '\'num_filters\': input_shape[1],'
-             '\'filter_size\': (convo_size, convo_size, convo_size),'
-             '\'pad\': \'full\'})'),
-        'r': ('reshape', '(ReshapeLayer, {'
-             '\'name\': \'\033[32mreshape\033[0m\','
-             '\'shape\': ([0], -1)})'),
-        's': ('3d_out', '(DenseLayer, {'
-             '\'name\':\'\033[32m3d_out\033[0m\','
-             '\'num_units\': reduce(mul, input_shape[2:], 1),'
-             '\'nonlinearity\': nonlinearities.softmax})'),
-        'p': ('class_out', '(DenseLayer, {'
-             '\'name\':\'\033[32mclass_out\033[0m\','
-             '\'num_units\': 2,'
-             '\'nonlinearity\': nonlinearities.softmax})')
+        'i': ('input',
+              '(InputLayer, {'
+              '\'name\': \'\033[30minput\033[0m\','
+              '\'shape\': input_shape})'),
+        'c': ('\'conv%d\' % c_index.inc()',
+              '(Conv3DDNNLayer, {'
+              '\'name\': \'\033[34mconv%d\033[0m\' % (c_index.inc()),'
+              '\'num_filters\': number_filters,'
+              '\'filter_size\': (convo_size, convo_size, convo_size),'
+              '\'pad\': \'valid\'})'),
+        'a': ('\'avg_pool%d\' % p_index.inc()',
+              '(Pool3DDNNLayer, {'
+              '\'name\': \'\033[31mavg_pool%d\033[0m\' % (p_index.inc()),'
+              '\'pool_size\': pool_size,'
+              '\'mode\': \'average_inc_pad\'})'),
+        'm': ('\'max_pool%d\' % p_index.inc()',
+              '(MaxPool3DDNNLayer, {'
+              '\'name\': \'\033[31mmax_pool%d\033[0m\' % (p_index.inc()),'
+              '\'pool_size\': pool_size})'),
+        'u': ('\'unpool%d\' % p_index.dec()',
+              '(Unpooling3D, {'
+              '\'name\': \'\033[35munpool%d\033[0m\' % (p_index.dec()),'
+              '\'pool_size\': pool_size})'),
+        'd': ('\'deconv%d\' % c_index.dec()',
+              '(Conv3DDNNLayer, {'
+              '\'name\': \'\033[36mdeconv%d\033[0m\' % (c_index.dec()),'
+              '\'num_filters\': number_filters,'
+              '\'filter_size\': (convo_size, convo_size, convo_size),'
+              '\'pad\': \'full\'})'),
+        'f': ('final',
+              '(Conv3DDNNLayer, {'
+              '\'name\': \'\033[36mfinal\033[0m\','
+              '\'num_filters\': input_shape[1],'
+              '\'filter_size\': (convo_size, convo_size, convo_size),'
+              '\'pad\': \'full\'})'),
+        'r': ('reshape',
+              '(ReshapeLayer, {'
+              '\'name\': \'\033[32mreshape\033[0m\','
+              '\'shape\': ([0], -1)})'),
+        's': ('3d_out',
+              '(DenseLayer, {'
+              '\'name\':\'\033[32m3d_out\033[0m\','
+              '\'num_units\': reduce(mul, input_shape[2:], 1),'
+              '\'nonlinearity\': nonlinearities.softmax})'),
+        'p': ('class_out',
+              '(DenseLayer, {'
+              '\'name\':\'\033[32mclass_out\033[0m\','
+              '\'num_units\': 2,'
+              '\'nonlinearity\': nonlinearities.softmax})')
     }
 
-    return [eval(layer) for l in net_layers for name, layer in possible_layers[l]]
+    layers_list_no_short = [eval(layer) for l in net_layers for name, layer in possible_layers[l]]
+    layers_dict = dict([(eval(name), eval(layer)) for l in net_layers for name, layer in possible_layers[l]])
+
+    return layers_dict if shortcuts else layers_list_no_short
+
+
+def create_classifier_net(layers, input_shape, convo_size, pool_size, number_filters, dir_name):
+    NeuralNet(
+        layers=get_layers_string(layers, input_shape, convo_size, pool_size, number_filters),
+
+        regression=False,
+        objective_loss_function=objectives.categorical_crossentropy,
+
+        update=updates.adadelta,
+        # update=updates.adam,
+        # update_learning_rate=1e-3,
+
+        on_epoch_finished=get_epoch_finished(os.path.join(dir_name, 'patches')),
+
+        verbose=11,
+        max_epochs=200
+    )
+
+
+def create_cnn3d_det_string(cnn_path, input_shape, convo_size, pool_size, number_filters, dir_name):
+    # We create the final string defining the net with the necessary input and reshape layers
+    # We assume that the user will never put these parameters as part of the net definition when
+    # calling the main python function
+    final_layers = 'i' + cnn_path + 'r' + 'p'
+
+    return create_classifier_net(final_layers, input_shape, convo_size, pool_size, number_filters, dir_name)
+
+
+def create_unet3d_seg_string(forward_path, input_shape, convo_size, pool_size, number_filters, dir_name):
+    # We create the final string defining the net with the necessary input and reshape layers
+    # We assume that the user will never put these parameters as part of the net definition when
+    # calling the main python function
+    final_layers = 'i' + forward_path + get_back_pathway(forward_path) + 'r' + 's'
+
+    return create_classifier_net(final_layers, input_shape, convo_size, pool_size, number_filters, dir_name)
+
+
+def create_unet3d_det_string(forward_path, input_shape, convo_size, pool_size, number_filters, dir_name):
+    # We create the final string defining the net with the necessary input and reshape layers
+    # We assume that the user will never put these parameters as part of the net definition when
+    # calling the main python function
+    final_layers = 'i' + forward_path + get_back_pathway(forward_path) + 'r' + 'p'
+
+    return create_classifier_net(final_layers, input_shape, convo_size, pool_size, number_filters, dir_name)
 
 
 def create_encoder3d_string(forward_path, input_shape, convo_size, pool_size, number_filters, dir_name):
@@ -117,80 +175,6 @@ def create_encoder3d_string(forward_path, input_shape, convo_size, pool_size, nu
         # update_learning_rate=1e-3,
 
         on_epoch_finished=get_epoch_finished(os.path.join(dir_name, 'encoder')),
-
-        verbose=11,
-        max_epochs=200
-    )
-
-    return encoder
-
-
-def create_cnn3d_det_string(cnn_path, input_shape, convo_size, pool_size, number_filters, dir_name):
-    # We create the final string defining the net with the necessary input and reshape layers
-    # We assume that the user will never put these parameters as part of the net definition when
-    # calling the main python function
-    final_layers = 'i' + cnn_path + 'r' + 'p'
-
-    encoder = NeuralNet(
-        layers=get_layers_string(final_layers, input_shape, convo_size, pool_size, number_filters),
-
-        regression=True,
-
-        update=updates.adadelta,
-        # update=updates.adam,
-        # update_learning_rate=1e-3,
-
-        on_epoch_finished=get_epoch_finished(os.path.join(dir_name, 'encoder')),
-
-        verbose=11,
-        max_epochs=200
-    )
-
-    return encoder
-
-
-def create_unet3d_seg_string(forward_path, input_shape, convo_size, pool_size, number_filters, dir_name):
-    # We create the final string defining the net with the necessary input and reshape layers
-    # We assume that the user will never put these parameters as part of the net definition when
-    # calling the main python function
-    final_layers = 'i' + forward_path + get_back_pathway(forward_path) + 'r' + 's'
-
-    encoder = NeuralNet(
-        layers=get_layers_string(final_layers, input_shape, convo_size, pool_size, number_filters),
-
-        regression=False,
-        objective_loss_function=objectives.categorical_crossentropy,
-
-         update=updates.adadelta,
-        # update=updates.adam,
-        # update_learning_rate=1e-3,
-
-        on_epoch_finished=get_epoch_finished(os.path.join(dir_name, 'unet')),
-
-        verbose=11,
-        max_epochs=200
-    )
-
-    return encoder
-
-
-def create_unet3d_det_string(forward_path, input_shape, convo_size, pool_size, number_filters, dir_name):
-    # We create the final string defining the net with the necessary input and reshape layers
-    # We assume that the user will never put these parameters as part of the net definition when
-    # calling the main python function
-    final_layers = 'i' + forward_path + get_back_pathway(forward_path) + 'r' + 'p'
-
-    encoder = NeuralNet(
-        layers=get_layers_string(final_layers, input_shape, convo_size, pool_size, number_filters),
-
-        regression=False,
-        objective_loss_function=objectives.categorical_crossentropy,
-
-        update=updates.adadelta,
-        # update=updates.adam,
-        # update_learning_rate=1e-3,
-
-        on_epoch_finished=get_epoch_finished(os.path.join(dir_name, 'patches')),
 
         verbose=11,
         max_epochs=200
