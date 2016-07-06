0a3a53888efbfb89cb1c5359873c8a6f75c8207e
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-07-06 17:18:01 +0200

    - DSC v4

--- nets.py
+++ nets.py
@@ -15,8 +15,8 @@ from lasagne import nonlinearities
 
 
 def probabilistic_dsc_objective(predictions, targets):
-    print predictions.shape
-    print targets.shape
+    print '---- Predictions shape = (' + ','.join([str(length) for length in predictions.shape]) + ')'
+    print '---- Targets shape = (' + ','.join([str(length) for length in targets.shape]) + ')'
     top = 2 * tensor.sum(predictions[1,:] * targets, axis=predictions.ndim - 1)
     bottom = tensor.sum(predictions[1,:] + targets, axis=predictions.ndim - 1)
     return 1.0 - (top / bottom)
