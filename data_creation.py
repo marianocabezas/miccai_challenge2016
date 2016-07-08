d03dfaf98013f9f45c45c180c31a33623349e768
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-07-08 18:05:00 +0200

    - Changed threshold for ROIs

--- data_creation.py
+++ data_creation.py
@@ -283,7 +283,7 @@ def load_patches(
 
     # We load the image modalities for each patient according to the parameters
     rois = load_thresholded_images(roi_names, dir_name, threshold=0.5) if roi_names \
-        else load_thresholded_images(flair_name, dir_name, threshold=1.5)
+        else load_thresholded_images(flair_name, dir_name, threshold=1)
     if use_flair:
         print 'Loading ' + flair_name + ' images'
         flair, y, flair_names = load_patch_vectors(flair_name, mask_name, dir_name, size, rois, random_state)
