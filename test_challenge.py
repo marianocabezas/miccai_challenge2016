d2a642a23b922c4efe61e9eb8f6af9c10637f64f
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-07-13 11:33:34 +0200

    - CHanged shifting

--- test_challenge.py
+++ test_challenge.py
@@ -119,8 +119,8 @@ def main():
     print(c['c'] + '[' + strftime("%H:%M:%S") + '] ' + c['g'] +
           '<Saving to file ' + c['b'] + options['output'] + c['nc'] + c['g'] + '>' + c['nc'])
     image = (image1 * image2) > 0.5
-    # image_nii.get_data()[:] = np.roll(np.roll(image, -1, axis=0), -1, axis=1)
-    image_nii.get_data()[:] = image
+    image_nii.get_data()[:] = np.roll(np.roll(image, 1, axis=0), 1, axis=1)
+    # image_nii.get_data()[:] = image
     image_nii.to_filename(options['output'])
 
 
