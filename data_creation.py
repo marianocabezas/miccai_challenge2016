61a8262f66800d59324de3bf24c5be04379fcc98
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-07-13 17:34:50 +0200

    - Removed prints

--- data_creation.py
+++ data_creation.py
@@ -113,8 +113,6 @@ def load_thresholded_images(name, dir_name, threshold=2.0, datatype=np.float32):
     image_names = [os.path.join(dir_name, patient, name) for patient in patients]
     images = [load_nii(image_name).get_data() for image_name in image_names]
     rois = [image.astype(dtype=datatype) > threshold for image in images]
-    for roi, image in zip(rois, images):
-        print 'Mask size = ' + str(np.sum(roi)) + ' (min = ' + str(image.min()) + ', max = ' + str(image.max()) + ')'
     return rois
 
 
@@ -124,8 +122,6 @@ def load_thresholded_norm_images(name, dir_name, threshold=2.0, datatype=np.floa
     images = [load_nii(image_name).get_data() for image_name in image_names]
     images_norm = [(im.astype(dtype=datatype) - im[np.nonzero(im)].mean()) / im[np.nonzero(im)].std() for im in images]
     rois = [image > threshold for image in images_norm]
-    for roi, image in zip(rois, images_norm):
-        print 'Mask size = ' + str(np.sum(roi)) + ' (min = ' + str(image.min()) + ', max = ' + str(image.max()) + ')'
     return rois
 
 
