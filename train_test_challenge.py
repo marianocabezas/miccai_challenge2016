bf4d021df8b797db835ffe1512a03da6903acbe3
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-07-18 11:43:13 +0200

    - Fixed print

--- train_test_challenge.py
+++ train_test_challenge.py
@@ -127,7 +127,7 @@ def main():
         ''' Here we get the seeds '''
         print(c['c'] + '[' + strftime("%H:%M:%S") + ']    ' +
               c['g'] + '<Looking for seeds>' + c['nc'])
-        for patient in np.concatenate([names[:, :i], names[:, i+1:]], axis=1):
+        for patient in np.rollaxis(np.concatenate([names[:, :i], names[:, i+1:]], axis=1), 1):
             print(patient.shape)
             output_name = os.path.join('/'.join(patient[0].rsplit('/')[:-1]), 'test' + str(i) + '.iter1.nii.gz')
             try:
