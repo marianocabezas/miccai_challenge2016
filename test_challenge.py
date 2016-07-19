dfbd86b297809ad8b5cf9e4b2c37f88edc30c3d7
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-07-19 16:25:29 +0200

    - Modularized the code. Since the challenge is "done" I have moved all the CNN code to a new python module decoupled from the challenge.

--- test_challenge.py
+++ test_challenge.py
@@ -44,7 +44,7 @@ def main():
     print(c['c'] + '[' + strftime("%H:%M:%S") + '] ' + c['g'] +
           '<Loading the net ' + c['b'] + '1' + c['nc'] + c['g'] + '>' + c['nc'])
     net_name = '/usr/local/nets/deep-challenge2016.init.model_weights.pkl' if options['docker'] \
-        else '/home/sergivalverde/w/CNN/code/CNN1/miccai_challenge2016/deep-challenge2016.init.model_weights.pkl'
+        else './deep-challenge2016.init.model_weights.pkl'
     net = NeuralNet(
         layers=[
             (InputLayer, dict(name='in', shape=(None, 4, 15, 15, 15))),
@@ -83,7 +83,7 @@ def main():
     print(c['c'] + '[' + strftime("%H:%M:%S") + '] ' + c['g'] +
           '<Loading the net ' + c['b'] + '2' + c['nc'] + c['g'] + '>' + c['nc'])
     net_name = '/usr/local/nets/deep-challenge2016.final.model_weights.pkl' if options['docker'] \
-        else '/home/sergivalverde/w/CNN/code/CNN1/miccai_challenge2016/deep-challenge2016.final.model_weights.pkl'
+        else './deep-challenge2016.final.model_weights.pkl'
     net = NeuralNet(
         layers=[
             (InputLayer, dict(name='in', shape=(None, 4, 15, 15, 15))),
